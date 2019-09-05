082c48869167f6fdf16caa0ccd22d1caaf675df1
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-04 22:07:38 -0300

    ejercicio python

---
+++ cuadrado.py
@@ -0,0 +1,14 @@
+import turtle
+
+window=turtle.Screen()
+said=turtle.Turtle()
+said.forward(50)
+said.left(90)
+said.forward(50)
+said.left(90)
+said.forward(50)
+said.left(90)
+said.forward(50)
+said.left(90)
+
+turtle.mainloop()
