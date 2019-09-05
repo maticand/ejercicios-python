0ed10eefed53af77ef500e91b9a862f06f465f47
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-04 22:08:52 -0300

    ejercicio python

---
+++ estrella.py
@@ -0,0 +1,19 @@
+import turtle
+
+from turtle import *
+color('red', 'yellow')
+window = turtle.Screen()
+froggie = turtle.Turtle()
+froggie.left(-108)
+froggie.forward(90)
+froggie.left(144)
+froggie.forward(90)
+froggie.left(144)
+froggie.forward(90)
+froggie.left(144)
+froggie.forward(90)
+froggie.left(144)
+froggie.forward(90)
+froggie.left(72)
+froggie.circle(45)
+turtle.mainloop()
