03771a17a32fe5e28e16477654cca058326b55ff
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-11 18:32:07 -0300

    ejercicio python

---
+++ ejercicio_5.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# imprimir los números impares hasta el 100 e imprimir cuantos impares hay
+
+i = 1
+cantidad_impares = 0
+
+while i < 99:
+    print(i)
+    i = i + 2
+    cantidad_impares = cantidad_impares + 1
+
+print("El número de impares es: ")
+print(cantidad_impares)
